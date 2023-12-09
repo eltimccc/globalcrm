9560340aa17576450ef7d544098d5c82d2361ce3
Denis M <eltimccc@gmail.com>
2023-12-09 19:59:31 +0300

    edit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # globalcrm
 
- docker-compose up -d --build
+docker-compose up -d --build
--- globalcrm/tasks/views.py
+++ globalcrm/tasks/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views import View
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
