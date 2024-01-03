180ff4c43116af5b5c3d1c7f7546ad820acca174
Denis M <eltimccc@gmail.com>
2024-01-04 01:16:42 +0300

    БД постгрес теперь в  докере для разработки

--- globalcrm/globalcrm/settings.py
+++ globalcrm/globalcrm/settings.py
@@ -4,24 +4,16 @@ from pathlib import Path
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-hz2xhua15lq6br#s62b!67v&mqcq%dibre0(q$h0n&xt8=d7v1"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
 
 ALLOWED_HOSTS = ["*"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -76,31 +68,19 @@ TEMPLATES = [
 WSGI_APPLICATION = "globalcrm.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', default="django.db.backends.postgresql"),
-#         'NAME': os.getenv('DB_NAME', default="postgres"),
-#         'USER': os.getenv('DB_USER', default="postgres"),
-#         'PASSWORD': os.getenv('DB_PASSWORD', default="postgres"),
-#         'HOST': os.getenv('DB_HOST', default="db"),
-#         'PORT': os.getenv('DB_PORT', default="5432")
-#     }
-# }
-
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = "/"
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -117,10 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 USE_TZ = True
 TIME_ZONE = "Europe/Moscow"
@@ -128,17 +104,10 @@ DATETIME_FORMAT = "d.m.Y H:i"
 
 USE_I18N = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_ROOT = "staticfiles"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
